cf412676864487191b8bea25bda3f630510b9df7
francesco.sovrano <toor@MacBook-Pro-di-Toor.local>
2020-12-14 10:40:36 +0100

    Update buffer.py

--- package/xarl/experience_buffers/buffer/buffer.py
+++ package/xarl/experience_buffers/buffer/buffer.py
@@ -37,41 +37,41 @@ class Buffer(object):
 			return result
 		return self.batches[self.get_type(type_id)]
 
-	def has_atleast(self, frames, type=None):
-		return self.count(type) >= frames
+	def has_atleast(self, frames, type_=None):
+		return self.count(type_) >= frames
 		
-	def has(self, frames, type=None):
-		return self.count(type) == frames
+	def has(self, frames, type_=None):
+		return self.count(type_) == frames
 		
-	def count(self, type=None):
-		if type is None:
+	def count(self, type_=None):
+		if type_ is None:
 			if len(self.batches) == 0:
 				return 0
 			return sum(len(batch) for batch in self.batches)
-		return len(self.batches[type])
+		return len(self.batches[type_])
 		
 	def id_is_full(self, type_id):
 		return self.has(self.size, self.get_type(type_id))
 		
-	def is_full(self, type=None):
-		if type is None:
+	def is_full(self, type_=None):
+		if type_ is None:
 			return self.has(self.size*len(self.types))
-		return self.has(self.size, type)
+		return self.has(self.size, type_)
 		
-	def is_empty(self, type=None):
-		return not self.has_atleast(1, type)
+	def is_empty(self, type_=None):
+		return not self.has_atleast(1, type_)
 		
 	def get_type(self, type_id):
 		return self.types[type_id]
 
 	def add(self, batch, type_id=0): # put batch into buffer
 		self._add_type_if_not_exist(type_id)
-		type = self.get_type(type_id)
-		self.batches[type].append(batch)
+		type_ = self.get_type(type_id)
+		self.batches[type_].append(batch)
 
 	def sample(self, remove=False):
-		type = choice(self.type_values)
-		result = choice(self.batches[type])
+		type_ = choice(self.type_values)
+		result = choice(self.batches[type_])
 		if remove:
-			self.batches[type].remove(result)
+			self.batches[type_].remove(result)
 		return result
