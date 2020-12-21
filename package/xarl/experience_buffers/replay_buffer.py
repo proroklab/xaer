99d829966a1e637cdc7d321ca5fed451a1bb24a2
francesco.sovrano <toor@MacBook-Pro-di-Toor.local>
2020-12-21 09:16:06 +0100

    Update replay_buffer.py

--- package/xarl/experience_buffers/replay_buffer.py
+++ package/xarl/experience_buffers/replay_buffer.py
@@ -36,7 +36,7 @@ class LocalReplayBuffer(ParallelIteratorWorker):
 		self.prioritized_replay = prioritized_replay
 		self.buffer_options = {} if not buffer_options else buffer_options
 		self.replay_starts = learning_starts
-		# self._buffer_lock = threading.Lock() 
+		self._buffer_lock = threading.Lock() 
 
 		ParallelIteratorWorker.__init__(self, None, False)
 
@@ -63,19 +63,19 @@ class LocalReplayBuffer(ParallelIteratorWorker):
 		if isinstance(batch, SampleBatch):
 			batch = MultiAgentBatch({DEFAULT_POLICY_ID: batch}, batch.count)
 		with self.add_batch_timer:
-			# with self._buffer_lock:
-			for policy_id, b in batch.policy_batches.items():
-				self.num_added += 1
-				if isinstance(batch_type,(tuple,list)):
-					for sub_batch_type in batch_type:
-						if self.update_only_sampled_cluster:
-							b_copy = b.copy()
-							b_copy['infos'] = copy.deepcopy(b_copy['infos'])
-							self.replay_buffers[policy_id].add(b_copy, sub_batch_type)
-						else:
-							self.replay_buffers[policy_id].add(b, sub_batch_type)
-				else:
-					self.replay_buffers[policy_id].add(b, batch_type)
+			with self._buffer_lock:
+				for policy_id, b in batch.policy_batches.items():
+					self.num_added += 1
+					if isinstance(batch_type,(tuple,list)):
+						for sub_batch_type in batch_type:
+							if self.update_only_sampled_cluster:
+								b_copy = b.copy()
+								b_copy['infos'] = copy.deepcopy(b_copy['infos'])
+								self.replay_buffers[policy_id].add(b_copy, sub_batch_type)
+							else:
+								self.replay_buffers[policy_id].add(b, sub_batch_type)
+					else:
+						self.replay_buffers[policy_id].add(b, batch_type)
 			return batch
 
 	def can_replay(self):
@@ -89,15 +89,15 @@ class LocalReplayBuffer(ParallelIteratorWorker):
 			return None
 
 		with self.replay_timer:
-			# with self._buffer_lock:
-			batch_list = [{} for _ in range(replay_size)]
-			for policy_id, replay_buffer in self.replay_buffers.items():
-				batch_iter = gen_sample(replay_buffer) # generate new batches
-				if filter_duplicates:
-					batch_iter = unique_everseen(batch_iter, key=lambda x:sorted(x['infos'][0]["batch_index"].items())) # skip duplicates
-				batch_iter = islice(batch_iter, replay_size) # take the first n batches
-				for i,batch in enumerate(batch_iter):
-					batch_list[i][policy_id] = batch
+			with self._buffer_lock:
+				batch_list = [{} for _ in range(replay_size)]
+				for policy_id, replay_buffer in self.replay_buffers.items():
+					batch_iter = gen_sample(replay_buffer) # generate new batches
+					if filter_duplicates:
+						batch_iter = unique_everseen(batch_iter, key=lambda x:sorted(x['infos'][0]["batch_index"].items())) # skip duplicates
+					batch_iter = islice(batch_iter, replay_size) # take the first n batches
+					for i,batch in enumerate(batch_iter):
+						batch_list[i][policy_id] = batch
 		return [
 			MultiAgentBatch(samples, max(map(lambda x:x.count, samples.values())))
 			for samples in batch_list
@@ -105,13 +105,13 @@ class LocalReplayBuffer(ParallelIteratorWorker):
 
 	def update_priorities(self, prio_dict):
 		with self.update_priorities_timer:
-			# with self._buffer_lock:
-			for policy_id, new_batch in prio_dict.items():
-				# Make a copy so the replay buffer doesn't pin plasma memory.
-				new_batch = new_batch.copy()
-				new_batch['infos'] = copy.deepcopy(new_batch['infos'])
-				for type_id,batch_index in new_batch['infos'][0]["batch_index"].items():
-					self.replay_buffers[policy_id].update_priority(new_batch, batch_index, type_id)
+			with self._buffer_lock:
+				for policy_id, new_batch in prio_dict.items():
+					# Make a copy so the replay buffer doesn't pin plasma memory.
+					new_batch = new_batch.copy()
+					new_batch['infos'] = copy.deepcopy(new_batch['infos'])
+					for type_id,batch_index in new_batch['infos'][0]["batch_index"].items():
+						self.replay_buffers[policy_id].update_priority(new_batch, batch_index, type_id)
 
 	def stats(self, debug=False):
 		stat = {
