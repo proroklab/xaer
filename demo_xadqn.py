1) Added max_tree to sum segment trees, to keep track of maximum values. 2) Fix during buffer instantiation: wrong option's name. 3) Added eta-weighting for priorities lower than zero (e.g. XAPPO's). 4) Added option to control priorities updates in XAPPO. 5) Fix in XAPPO's post-processing function: now using old actions instead of predicting new ones. 6) Changes to default demos' options.
1) Added max_tree to sum segment trees, to keep track of maximum values.
2) Fix during buffer instantiation: wrong option's name.
3) Added eta-weighting for priorities lower than zero (e.g. XAPPO's).
4) Added option to control priorities updates in XAPPO.
5) Fix in XAPPO's post-processing function: now using old actions instead of predicting new ones.
6) Changes to default demos' options.

demo_xadqn.py
@@ -42,11 +42,12 @@ CONFIG.update({
 	########################################################
 	"buffer_options": {
 		'priority_id': "td_errors", # Which batch column to use for prioritisation. Default is inherited by DQN and it is 'td_errors'. One of the following: rewards, prev_rewards, td_errors.
-		'priority_aggregation_fn': 'lambda x: np.mean(np.abs(x))', # A reduce function that takes as input a list of numbers and returns a number representing a batch priority.
+		'priority_aggregation_fn': 'lambda x: np.mean(np.abs(x))', # A reduction that takes as input a list of numbers and returns a number representing a batch priority.
 		'cluster_size': None, # Default None, implying being equal to global_size. Maximum number of batches stored in a cluster (which number depends on the clustering scheme) of the experience buffer. Every batch has size 'replay_sequence_length' (default is 1).
 		'global_size': 2**15, # Default 50000. Maximum number of batches stored in all clusters (which number depends on the clustering scheme) of the experience buffer. Every batch has size 'replay_sequence_length' (default is 1).
 		'alpha': 0.6, # How much prioritization is used (0 - no prioritization, 1 - full prioritization).
-		'beta': 0.4, # Parameter that regulates a mechanism for computing importance sampling.
+		'beta': 0.4, # To what degree to use importance weights (0 - no corrections, 1 - full correction).
+		'eta': None, # A value > 0 that enables eta-weighting, thus allowing for importance weighting with priorities lower than 0. Eta is used to avoid importance weights equal to 0 when the sampled batch is the one with the highest priority. The closer eta is to 0, the closer to 0 would be the importance weight of the highest-priority batch.
 		'epsilon': 1e-6, # Epsilon to add to a priority so that it is never equal to 0.
 		'prioritized_drop_probability': 0, # Probability of dropping the batch having the lowest priority in the buffer instead of the one having the lowest timestamp. In DQN default is 0.
 		'update_insertion_time_when_sampling': False, # Whether to update the insertion time batches to the time of sampling. It requires prioritized_drop_probability < 1. In DQN default is False.

package/xarl/agents/xappo.py
@@ -24,20 +24,22 @@ XAPPO_EXTRA_OPTIONS = {
 	"vtrace": False, # Formula for computing the advantages: batch_mode==complete_episodes implies vtrace==False, thus gae==True.
 	"replay_proportion": 2, # Set a p>0 to enable experience replay. Saved samples will be replayed with a p:1 proportion to new data samples.
 	##########################################
-	"gae_with_vtrace": False, # Formula for computing the advantages: combines GAE with V-Trace, for better sample efficiency.
-	"prioritized_replay": True,
+	"gae_with_vtrace": False, # Formula for computing the advantages: it combines GAE with V-Trace, for better sample efficiency.
+	"prioritized_replay": True, # Whether to replay batches with the highest priority/importance/relevance for the agent.
+	"update_priorities": True, # Whether to update priorities when replaying.
 	"learning_starts": 100, # How many batches to sample before learning starts. Every batch has size 'rollout_fragment_length' (default is 50).
 	"buffer_options": {
 		'priority_id': "gains", # Which batch column to use for prioritisation. One of the following: gains, importance_weights, unweighted_advantages, advantages, rewards, prev_rewards, action_logp.
-		'priority_aggregation_fn': 'np.sum', # A reduce function that takes as input a list of numbers and returns a number representing a batch priority.
+		'priority_aggregation_fn': 'np.sum', # A reduction that takes as input a list of numbers and returns a number representing a batch priority.
 		'cluster_size': None, # Maximum number of batches stored in a cluster (which number depends on the clustering scheme) of the experience buffer. Every batch has size 'rollout_fragment_length' (default is 50).
 		'global_size': 2**9, # Maximum number of batches stored in all clusters (which number depends on the clustering scheme) of the experience buffer. Every batch has size 'rollout_fragment_length' (default is 50).
 		'alpha': 0.5, # How much prioritization is used (0 - no prioritization, 1 - full prioritization).
-		'beta': None, # Parameter that regulates a mechanism for computing importance sampling; PPO probably does not need it because it has another mechanism for importance weighting.
+		'beta': None, # To what degree to use importance weights (0 - no corrections, 1 - full correction).
+		'eta': 1e-2, # A value > 0 that enables eta-weighting, thus allowing for importance weighting with priorities lower than 0 if beta is > 0. Eta is used to avoid importance weights equal to 0 when the sampled batch is the one with the highest priority. The closer eta is to 0, the closer to 0 would be the importance weight of the highest-priority batch.
 		'epsilon': 1e-6, # Epsilon to add to a priority so that it is never equal to 0.
 		'prioritized_drop_probability': 0, # Probability of dropping the batch having the lowest priority in the buffer.
 		'update_insertion_time_when_sampling': False, # Whether to update the insertion time batches to the time of sampling. It requires prioritized_drop_probability < 1. In DQN default is False.
-		'global_distribution_matching': False, # Whether to use a random number rather than the batch priority during prioritised dropping. If True then: At time t the probability of any experience being the max experience is 1/t regardless of when the sample was added, guaranteeing that (when prioritized_drop_probability==1) at any given time the sampled experiences will approximately match the distribution of all samples seen so far.
+		'global_distribution_matching': True, # Whether to use a random number rather than the batch priority during prioritised dropping. If True then: At time t the probability of any experience being the max experience is 1/t regardless of when the sample was added, guaranteeing that (when prioritized_drop_probability==1) at any given time the sampled experiences will approximately match the distribution of all samples seen so far.
 		'prioritised_cluster_sampling_strategy': 'above_average', # Whether to select which cluster to replay in a prioritised fashion -- 4 options: None; 'highest' - clusters with the highest priority are more likely to be sampled; 'average' - prioritise the cluster with priority closest to the average cluster priority; 'above_average' - prioritise the cluster with priority closest to the cluster with the smallest priority greater than the average cluster priority.
 		'cluster_level_weighting': False, # Whether to use only cluster-level information to compute importance weights rather than the whole buffer.
 	},
@@ -104,11 +106,15 @@ def compute_gae_v_advantages(rollout: SampleBatch, last_r: float, gamma: float =
 
 def xappo_postprocess_trajectory(policy, sample_batch, other_agent_batches=None, episode=None):
 	# Add PPO's importance weights
-	_,_,actions_info = policy.compute_actions_from_input_dict(sample_batch, episodes=[episode])
-	action_logp = actions_info['action_logp']
+	action_logp = policy.compute_log_likelihoods(
+		actions=sample_batch[SampleBatch.ACTIONS],
+		obs_batch=sample_batch[SampleBatch.CUR_OBS],
+		state_batches=None, # missing, needed for RNN-based models
+		prev_action_batch=None,
+		prev_reward_batch=None,
+	)
 	old_action_logp = sample_batch[SampleBatch.ACTION_LOGP]
-	logp_ratio = np.exp(action_logp - old_action_logp)
-	sample_batch["importance_weights"] = logp_ratio
+	sample_batch["importance_weights"] = np.exp(action_logp - old_action_logp)
 	# Add advantages, after "importance_weights"
 	completed = sample_batch["dones"][-1]
 	if completed:
@@ -132,16 +138,11 @@ def xappo_postprocess_trajectory(policy, sample_batch, other_agent_batches=None,
 			use_gae=policy.config["use_gae"],
 			use_critic=policy.config["use_critic"]
 		)
+	# Add gains
+	sample_batch["gains"] = sample_batch[Postprocessing.ADVANTAGES] * sample_batch["importance_weights"]
 	sample_batch['unweighted_'+Postprocessing.ADVANTAGES] = sample_batch[Postprocessing.ADVANTAGES].copy()
 	if 'weights' in sample_batch:
-		# print(sample_batch['weights'])
 		sample_batch[Postprocessing.ADVANTAGES] *= sample_batch['weights']
-	# Add gains
-	advantages = sample_batch[Postprocessing.ADVANTAGES]
-	new_priorities = advantages * logp_ratio
-	sample_batch["gains"] = new_priorities
-	# if "new_obs" in sample_batch:
-	# 	del sample_batch.data["new_obs"]  # not used, so save some bandwidth
 	return sample_batch
 
 XAPPOTFPolicy = AsyncPPOTFPolicy.with_updates(
@@ -178,7 +179,7 @@ def xappo_execution_plan(workers, config):
 			local_buffer=local_replay_buffer,
 			replay_proportion=config["replay_proportion"],
 			cluster_overview_size=config["cluster_overview_size"],
-			update_replayed_fn=get_update_replayed_batch_fn(local_replay_buffer, local_worker, xappo_postprocess_trajectory),
+			update_replayed_fn=get_update_replayed_batch_fn(local_replay_buffer, local_worker, xappo_postprocess_trajectory) if config["update_priorities"] else lambda x:x,
 		)) \
 		.flatten() \
 		.combine(ConcatBatches(min_batch_size=config["train_batch_size"]))
@@ -192,9 +193,7 @@ def xappo_execution_plan(workers, config):
 	# Only need to update workers if there are remote workers.
 	if workers.remote_workers():
 		enqueue_op = enqueue_op.zip_with_source_actor() \
-			.for_each(BroadcastUpdateLearnerWeights(
-				learner_thread, workers,
-				broadcast_interval=config["broadcast_interval"]))
+			.for_each(BroadcastUpdateLearnerWeights(learner_thread, workers, broadcast_interval=config["broadcast_interval"]))
 
 	dequeue_op = Dequeue(learner_thread.outqueue, check=learner_thread.is_alive) \
 		.for_each(record_steps_trained)

package/xarl/experience_buffers/buffer/pseudo_prioritized_buffer.py
@@ -12,7 +12,7 @@ get_batch_indexes = lambda x: get_batch_infos(x)['batch_index']
 get_batch_uid = lambda x: get_batch_infos(x)['batch_uid']
 
 class PseudoPrioritizedBuffer(Buffer):
-	__slots__ = ('_priority_id','_priority_aggregation_fn','_alpha','_beta','_epsilon','_prioritized_drop_probability','_global_distribution_matching','_it_capacity','_sample_priority_tree','_drop_priority_tree','_insertion_time_tree','_prioritised_cluster_sampling','_prioritised_cluster_sampling_strategy','_update_insertion_time_when_sampling','_cluster_level_weighting')
+	__slots__ = ('_priority_id','_priority_aggregation_fn','_alpha','_beta','_teta','_epsilon','_prioritized_drop_probability','_global_distribution_matching','_it_capacity','_sample_priority_tree','_drop_priority_tree','_insertion_time_tree','_prioritised_cluster_sampling','_prioritised_cluster_sampling_strategy','_update_insertion_time_when_sampling','_cluster_level_weighting')
 	
 	def __init__(self, 
 		priority_id,
@@ -21,6 +21,7 @@ class PseudoPrioritizedBuffer(Buffer):
 		global_size=50000, 
 		alpha=0.6, 
 		beta=0.4, 
+		eta=1e-2,
 		epsilon=1e-6,
 		prioritized_drop_probability=0.5, 
 		global_distribution_matching=False, 
@@ -28,11 +29,13 @@ class PseudoPrioritizedBuffer(Buffer):
 		update_insertion_time_when_sampling=False,
 		cluster_level_weighting=True,
 	): # O(1)
+		assert not beta or beta > 0., f"beta must be > 0, but it is {beta}"
+		assert not eta or eta > 0, f"eta must be > 0, but it is {eta}"
 		self._priority_id = priority_id
 		self._priority_aggregation_fn = eval(priority_aggregation_fn)
 		self._alpha = alpha # How much prioritization is used (0 - no prioritization, 1 - full prioritization)
 		self._beta = beta # To what degree to use importance weights (0 - no corrections, 1 - full correction).
-		assert self._beta is None or self._beta > 0., "beta > 0, if beta is not None"
+		self._teta = 1 + eta if eta else None # Eta is a value > 0 that enables eta-weighting, thus allowing for importance weighting with priorities lower than 0. Eta is used to avoid importance weights equal to 0 when the sampled batch is the one with the highest priority. The closer eta is to 0, the closer to 0 would be the importance weight of the highest-priority batch.
 		self._epsilon = epsilon # Epsilon to add to the priorities when updating priorities.
 		self._prioritized_drop_probability = prioritized_drop_probability # remove the worst batch with this probability otherwise remove the oldest one
 		self._global_distribution_matching = global_distribution_matching
@@ -149,9 +152,9 @@ class PseudoPrioritizedBuffer(Buffer):
 			self._drop_priority_tree[sample_type][idx] = (random(), idx) # O(log)
 		# Set priority
 		self.update_priority(batch, idx, type_id) # add batch
-		# if self.global_size:
-		# 	assert self.count() <= self.global_size, 'Memory leak in replay buffer; v1'
-		# 	assert super().count() <= self.global_size, 'Memory leak in replay buffer; v2'
+		if self.global_size:
+			assert self.count() <= self.global_size, 'Memory leak in replay buffer; v1'
+			assert super().count() <= self.global_size, 'Memory leak in replay buffer; v2'
 		return idx, type_id
 
 	def sample_cluster(self):
@@ -175,8 +178,8 @@ class PseudoPrioritizedBuffer(Buffer):
 			worst_type_priority = np.min(type_priority)
 			type_cumsum = np.cumsum(type_priority-worst_type_priority) # O(|self.type_keys|)
 			type_mass = random() * type_cumsum[-1] # O(1)
-			assert 0 <= type_mass, 'type_mass is too small'
-			assert type_mass <= type_cumsum[-1], 'type_mass is too big'
+			assert 0 <= type_mass, f'type_mass {type_mass} should be greater than 0'
+			assert type_mass <= type_cumsum[-1], f'type_mass {type_mass} should be lower than {type_cumsum[-1]}'
 			tree_idx,_ = next(filter(lambda x: x[-1] >= type_mass, enumerate(type_cumsum))) # O(|self.type_keys|)
 			sample_type = tree_list[tree_idx][0]
 		else:
@@ -188,10 +191,13 @@ class PseudoPrioritizedBuffer(Buffer):
 		type_id, sample_type = self.sample_cluster()
 		type_sum_tree = self._sample_priority_tree[sample_type]
 		type_batch = self.batches[sample_type]
-		if self._beta is not None: # Update weights
+		if self._beta: # Update weights
 			if self._cluster_level_weighting: min_priority = type_sum_tree.min_tree.min()
 			else: min_priority = min(map(lambda x: x.min_tree.min(), self._sample_priority_tree))
-			assert min_priority > 0, "min_priority > 0, if beta is not None"
+			if not self._teta:
+				assert min_priority > 0, f"min_priority must be > 0, if beta is not None and eta is None, but it is {min_priority}"
+			if self._cluster_level_weighting: max_priority = type_sum_tree.max_tree.max()
+			else: max_priority = max(map(lambda x: x.max_tree.max(), self._sample_priority_tree))
 		batch_list = []
 		for _ in range(n):
 			idx = type_sum_tree.find_prefixsum_idx(prefixsum_fn=lambda mass: mass*random()) # O(log)
@@ -202,8 +208,13 @@ class PseudoPrioritizedBuffer(Buffer):
 					continue
 				self.remove_batch(self.get_type(other_type_id),other_idx)
 			# Update weights
-			if self._beta is not None: # Update weights
-				weight = (min_priority / type_sum_tree[idx])**self._beta
+			if self._beta: # Update weights
+				if self._teta:
+					weight = (max_priority*self._teta - type_sum_tree[idx])/(max_priority*self._teta - min_priority) # in (0,1] the closer is type_sum_tree[idx] to max_priority, the lower is the weight
+				else:
+					weight = (min_priority / type_sum_tree[idx])**self._beta # default, not compatible with negative priorities
+				weight = weight**self._beta
+				# print(weight, ((self._epsilon**self._alpha) / (type_sum_tree[idx]-min_priority))**self._beta, (min_priority / type_sum_tree[idx])**self._beta)
 				batch['weights'] = np.full(batch.count, weight, dtype=np.float32)
 			# Remove from buffer
 			if remove is True: