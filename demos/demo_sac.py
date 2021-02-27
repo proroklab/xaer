e26dbca77b7eb21dca9bb87e345c8592bf9021c7
francesco.sovrano <38495561+Francesco-Sovrano@users.noreply.github.com>
2021-02-27 09:30:20 +0100

    1) Changes to demos' default hyper-parameters.
    1) Changes to demos' default hyper-parameters.

--- demos/demo_sac.py
+++ demos/demo_sac.py
@@ -24,7 +24,7 @@ CONFIG.update({
 	"train_batch_size": 2**8, # Number of transitions per train-batch
 	"batch_mode": "truncate_episodes", # For some clustering schemes (e.g. extrinsic_reward, moving_best_extrinsic_reward, etc..) it has to be equal to 'complete_episodes', otherwise it can also be 'truncate_episodes'.
 	###########################
-	"tau": 5e-4, # The smaller tau, the lower the value over-estimation, the higher the bias
+	"tau": 1e-4, # The smaller tau, the lower the value over-estimation, the higher the bias
 	"prioritized_replay": True, # Whether to replay batches with the highest priority/importance/relevance for the agent.
 	'buffer_size': 2**14, # Size of the experience buffer. Default 50000
 	"prioritized_replay_alpha": 0.6,
