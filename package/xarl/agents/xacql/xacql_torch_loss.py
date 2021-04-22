677549d20d339800cfca949974c4d3264e25f539
francesco.sovrano <38495561+Francesco-Sovrano@users.noreply.github.com>
2021-04-22 14:33:40 +0200

    1) CQL already handles importance sampling, no need for beta weights.
    1) CQL already handles importance sampling, no need for beta weights.

--- package/xarl/agents/xacql/xacql_torch_loss.py
+++ package/xarl/agents/xacql/xacql_torch_loss.py
@@ -53,7 +53,8 @@ def cql_loss(policy, model, dist_class, train_batch):
 
 	# Unlike original SAC, Alpha and Actor Loss are computed first.
 	# Alpha Loss
-	alpha_loss = -torch.mean(train_batch[PRIO_WEIGHTS] * model.log_alpha * (log_pis_t + model.target_entropy).detach())
+	alpha_loss = -(model.log_alpha *
+				   (log_pis_t + model.target_entropy).detach()).mean()
 
 	# Policy Loss (Either Behavior Clone Loss or SAC Loss)
 	alpha = torch.exp(model.log_alpha)
@@ -61,34 +62,39 @@ def cql_loss(policy, model, dist_class, train_batch):
 		min_q = model.get_q_values(model_out_t, policy_t)
 		if twin_q:
 			min_q = torch.min(min_q, model.get_twin_q_values(model_out_t, policy_t))
-		actor_loss = torch.mean(train_batch[PRIO_WEIGHTS] * (alpha.detach() * log_pis_t - min_q))
+		actor_loss = (alpha.detach() * log_pis_t - min_q).mean()
 	else:
 		bc_logp = action_dist_t.logp(actions)
-		actor_loss = torch.mean(train_batch[PRIO_WEIGHTS] * (alpha * log_pis_t - bc_logp))
+		actor_loss = (alpha * log_pis_t - bc_logp).mean()
 
 	# Critic Loss (Standard SAC Critic L2 Loss + CQL Entropy Loss)
 	# SAC Loss
-	action_dist_tp1 = action_dist_class(model.get_policy_output(model_out_tp1), policy.model)
-	policy_tp1 = action_dist_tp1.sample() if not deterministic else action_dist_tp1.deterministic_sample()
+	action_dist_tp1 = action_dist_class(
+		model.get_policy_output(model_out_tp1), policy.model)
+	policy_tp1 = action_dist_tp1.sample() if not deterministic else \
+		action_dist_tp1.deterministic_sample()
 
 	# Q-values for the batched actions.
 	q_t = model.get_q_values(model_out_t, train_batch[SampleBatch.ACTIONS])
 	q_t = torch.squeeze(q_t, dim=-1)
 	if twin_q:
-		twin_q_t = model.get_twin_q_values(model_out_t, train_batch[SampleBatch.ACTIONS])
+		twin_q_t = model.get_twin_q_values(model_out_t,
+										   train_batch[SampleBatch.ACTIONS])
 		twin_q_t = torch.squeeze(twin_q_t, dim=-1)
 
 	# Target q network evaluation.
 	q_tp1 = policy.target_model.get_q_values(target_model_out_tp1, policy_tp1)
 	if twin_q:
-		twin_q_tp1 = policy.target_model.get_twin_q_values(target_model_out_tp1, policy_tp1)
+		twin_q_tp1 = policy.target_model.get_twin_q_values(
+			target_model_out_tp1, policy_tp1)
 		# Take min over both twin-NNs.
 		q_tp1 = torch.min(q_tp1, twin_q_tp1)
 	q_tp1 = torch.squeeze(input=q_tp1, dim=-1)
 	q_tp1 = (1.0 - terminals.float()) * q_tp1
 
 	# compute RHS of bellman equation
-	q_t_target = (rewards + (discount**policy.config["n_step"]) * q_tp1).detach()
+	q_t_target = (
+		rewards + (discount**policy.config["n_step"]) * q_tp1).detach()
 
 	# Compute the TD-error (potentially clipped), for priority replay buffer
 	base_td_error = torch.abs(q_t - q_t_target)
@@ -97,17 +103,19 @@ def cql_loss(policy, model, dist_class, train_batch):
 		td_error = 0.5 * (base_td_error + twin_td_error)
 	else:
 		td_error = base_td_error
-	critic_loss = [nn.MSELoss()(train_batch[PRIO_WEIGHTS] * q_t, train_batch[PRIO_WEIGHTS] * q_t_target)]
+	critic_loss = [nn.MSELoss()(q_t, q_t_target)]
 	if twin_q:
-		critic_loss.append(nn.MSELoss()(train_batch[PRIO_WEIGHTS] * twin_q_t, train_batch[PRIO_WEIGHTS] * q_t_target))
+		critic_loss.append(nn.MSELoss()(twin_q_t, q_t_target))
 
 	# CQL Loss (We are using Entropy version of CQL (the best version))
 	rand_actions = convert_to_torch_tensor(
-		torch.FloatTensor(actions.shape[0] * num_actions, actions.shape[-1]).uniform_(action_low, action_high),
-		policy.device
-	)
-	curr_actions, curr_logp = policy_actions_repeat(model, action_dist_class, obs, num_actions)
-	next_actions, next_logp = policy_actions_repeat(model, action_dist_class, next_obs, num_actions)
+		torch.FloatTensor(actions.shape[0] * num_actions,
+						  actions.shape[-1]).uniform_(action_low, action_high),
+		policy.device)
+	curr_actions, curr_logp = policy_actions_repeat(model, action_dist_class,
+													obs, num_actions)
+	next_actions, next_logp = policy_actions_repeat(model, action_dist_class,
+													next_obs, num_actions)
 
 	curr_logp = curr_logp.view(actions.shape[0], num_actions, 1)
 	next_logp = next_logp.view(actions.shape[0], num_actions, 1)
@@ -118,8 +126,10 @@ def cql_loss(policy, model, dist_class, train_batch):
 
 	if twin_q:
 		q2_rand = q_values_repeat(model, model_out_t, rand_actions, twin=True)
-		q2_curr_actions = q_values_repeat(model, model_out_t, curr_actions, twin=True)
-		q2_next_actions = q_values_repeat(model, model_out_t, next_actions, twin=True)
+		q2_curr_actions = q_values_repeat(
+			model, model_out_t, curr_actions, twin=True)
+		q2_next_actions = q_values_repeat(
+			model, model_out_t, next_actions, twin=True)
 
 	random_density = np.log(0.5**curr_actions.shape[-1])
 	cat_q1 = torch.cat([
@@ -132,14 +142,17 @@ def cql_loss(policy, model, dist_class, train_batch):
 			q2_curr_actions - curr_logp.detach()
 		], 1)
 
-	min_qf1_loss = torch.mean(train_batch[PRIO_WEIGHTS] * torch.logsumexp(cat_q1 / cql_temp, dim=1)) * min_q_weight * cql_temp
-	min_qf1_loss = min_qf1_loss - torch.mean(train_batch[PRIO_WEIGHTS] * q_t) * min_q_weight
+	min_qf1_loss = torch.logsumexp(
+		cat_q1 / cql_temp, dim=1).mean() * min_q_weight * cql_temp
+	min_qf1_loss = min_qf1_loss - q_t.mean() * min_q_weight
 	if twin_q:
-		min_qf2_loss = torch.mean(train_batch[PRIO_WEIGHTS] * torch.logsumexp(cat_q2 / cql_temp, dim=1)) * min_q_weight * cql_temp
-		min_qf2_loss = min_qf2_loss - torch.mean(train_batch[PRIO_WEIGHTS] * twin_q_t) * min_q_weight
+		min_qf2_loss = torch.logsumexp(
+			cat_q2 / cql_temp, dim=1).mean() * min_q_weight * cql_temp
+		min_qf2_loss = min_qf2_loss - twin_q_t.mean() * min_q_weight
 
 	if use_lagrange:
-		alpha_prime = torch.clamp(model.log_alpha_prime.exp(), min=0.0, max=1000000.0)[0]
+		alpha_prime = torch.clamp(
+			model.log_alpha_prime.exp(), min=0.0, max=1000000.0)[0]
 		min_qf1_loss = alpha_prime * (min_qf1_loss - target_action_gap)
 		if twin_q:
 			min_qf2_loss = alpha_prime * (min_qf2_loss - target_action_gap)
