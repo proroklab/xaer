6074b16007bc0a889c6d17c07d1df5c4dad08d26
francesco.sovrano <38495561+Francesco-Sovrano@users.noreply.github.com>
2021-07-19 19:59:42 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,9 +4,17 @@
 # source .env/bin/activate
 # echo 'Update the virtual environment'
 # pip install -U pip setuptools wheel
-echo "Install other environments' dependencies"
+echo "Installing other environments' dependencies.."
 pip install -r environments/requirements.txt
-echo 'Install XARL'
+echo 'Installing XARL..'
 pip install -e ./package # cmake is needed
 # echo 'Fixing environments rendering'
 # pip install pyglet==1.5.11 # fix for rendering environments
+echo 'Installing Atari ROMs..'
+mkdir Atari-ROM
+cd Atari-ROM
+wget http://www.atarimania.com/roms/Roms.rar
+unrar x Roms.rar
+unzip ROMS.zip
+python -m atari_py.import_roms ROMS
+cd ..
