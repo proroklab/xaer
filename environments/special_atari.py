f98f93e91b0bc723adbaa3cdfc04e6bf9356eb26
francesco.sovrano <38495561+Francesco-Sovrano@users.noreply.github.com>
2021-08-16 14:53:39 +0200

    Update special_atari.py

--- environments/special_atari.py
+++ environments/special_atari.py
@@ -3,18 +3,27 @@ import numpy as np
 
 class SpecialAtariEnv(AtariEnv):
 
-	# def reset(self):
-	# 	obs = super().reset()
-	# 	self.last_delta = None
-	# 	return obs
+	def reset(self):
+		obs = super().reset()
+		self.lives = self.ale.lives()
+		return obs
 	
 	def step(self, a):
+		old_lives = self.lives
 		old_ram = self._get_ram()
 		state, reward, terminal, info_dict = super().step(a)
 		new_ram = self._get_ram()
+		new_lives = self.ale.lives()
 
-		delta = new_ram-old_ram
-		info_dict['explanation'] = np.array2string(delta) if reward != 0 else 'no_reward'
+		lost_lives = old_lives-new_lives
+		explanation_list = [np.array2string(new_ram-old_ram) if reward != 0 or lost_lives != 0 else 'no_reward']
+		if reward != 0:
+			explanation_list.append('reward')
+		if lost_lives != 0:
+			explanation_list.append('lost_lives')
+
+		info_dict['explanation'] = explanation_list
+		self.lives = new_lives
 		# print(reward, terminal, delta)
 
 		return state, reward, terminal, info_dict
